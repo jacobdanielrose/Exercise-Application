106d00529fff494a20c0fb17989fd2482506e271
Jacob Rose <jacobdanielrose@gmail.com>
2021-06-14 15:19:18 +0200

    Connected backend express server to frontend angular app.

--- backend/app.js
+++ backend/app.js
@@ -2,14 +2,30 @@ const express = require('express');
 
 const app = express();
 
-app.use((req,res, next) => {
-  console.log('First middleware');
+app.use((req,res,next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS");
   next();
 });
 
-
-app.use((req,res) => {
-  res.send('Hello from express!');
+app.use("/posts",(req,res) => {
+  const posts = [
+    {
+    id: "fas2d3frwe",
+    title: "First server-side post",
+    content: "This is comming from the server"
+  },
+    {
+      id: "fas2d3frwe",
+      title: "Second server-side post",
+      content: "This is comming from the server"
+    }
+  ];
+  res.status(200).json({
+    message: "Posts fetched successfully!",
+    posts: posts
+  })
 });
 
 module.exports = app;
