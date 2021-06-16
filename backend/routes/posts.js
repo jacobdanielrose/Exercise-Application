41326f249f79db0983331769678f8ad1a9059574
Jacob Rose <jacobdanielrose@gmail.com>
2021-06-16 22:32:32 +0200

    update image almost complete. However, upon image update, database returns all null. must find error.

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,11 +1,34 @@
 const express = require('express');
+const multer = require('multer');
 
 const Post = require('../models/post');
 
 const router = express.Router();
 
+const MIME_TYPE_MAP = {
+  'image/png': 'png',
+  'image/jpeg': 'jpg',
+  'image/jpg': 'jpg'
+};
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    const isValid = MIME_TYPE_MAP[file.mimetype];
+    let error = new Error("Invalid MIME type");
+    if (isValid) {
+      error = null;
+    }
+    cb(error, "backend/images");
+  },
+  filename: (req, file, cb) => {
+    const name = file.originalname.toLowerCase().split(' ').join('-');
+    const ext = MIME_TYPE_MAP[file.mimetype];
+    cb(null, name + '-' + Date.now() + '.' + ext);
+  }
+});
+
 router.route("/")
-  .get((req,res) => {
+  .get((req, res, next) => {
     Post.find()
       .then(documents => {
         console.log(documents);
@@ -15,16 +38,20 @@ router.route("/")
         });
       });
   })
-  .post((req, res, next) => {
-      const post = new Post({
-        title: req.body.title,
-        content: req.body.content
-      });
-
+  .post(multer({storage: storage}).single("image"), (req, res, next) => {
+    const url = req.protocol + '://' + req.get("host");
+    const post = new Post({
+      title: req.body.title,
+      content: req.body.content,
+      imagePath: url + "/images/" + req.file.filename
+    });
     post.save().then(createdPost => {
       res.status(201).json({
         message: "Post added successfully",
-        postId: createdPost._id
+        post: {
+          ...createdPost,
+          id: createdPost._id
+        }
       });
     });
   });
@@ -39,16 +66,20 @@ router.route("/:id")
       }
     });
   })
-  .put((req, res, next) => {
-    const post = new Post({
-      _id: req.body.id,
+  .put(multer({storage: storage}).single("image"), (req, res, next) => {
+    let imagePath = req.body.imagePath;
+    if (req.file) {
+      const url = req.protocol + '://' + req.get("host");
+      imagePath = url + "/images/" + req.file.filename;
+    }
+
+    Post.updateOne({_id: req.params.id}, {
       title: req.body.title,
-      content: req.body.content
-    })
-    Post.updateOne({_id: req.params.id}, post).then((result) => {
-      console.log(result);
+      content: req.body.content,
+      imagePath: imagePath
+    }).then((result) => {
       res.status(200).json({message: "Update successful!"});
-    })
+    });
   })
   .delete((req, res, next) => {
     Post.deleteOne({_id: req.params.id})
