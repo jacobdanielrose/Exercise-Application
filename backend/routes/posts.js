89d81cf3f993f47d22de4dc67aef9d95d98a22d5
Jacob Rose <jacobdanielrose@gmail.com>
2021-06-16 17:30:32 +0200

    implemented image upload and validation.

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -30,6 +30,15 @@ router.route("/")
   });
 
 router.route("/:id")
+  .get((req, res, next) => {
+    Post.findById(req.params.id).then(post =>{
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res.status(404).json({ message: "Post not found!"});
+      }
+    });
+  })
   .put((req, res, next) => {
     const post = new Post({
       _id: req.body.id,
