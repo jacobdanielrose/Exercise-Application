5ca7a38c86b1907b15dcbb2ff5813d5e0ae82e6f
Jacob Rose <jacobdanielrose@gmail.com>
2021-06-21 17:46:23 +0200

    need to fix form errors.

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
 const router = express.Router();
 
-router.route("/login")
+router.route("/signup")
   .post((req, res, next) => {
     bcrypt.hash(req.body.password, 10)
       .then(hash => {
@@ -29,9 +30,39 @@ router.route("/login")
       });
   });
 
-router.route("/signup")
+router.route("/login")
   .post((req, res, next) => {
-    email: req.body.email
+    let fetchedUser;
+    User.findOne( { email: req.body.email })
+      .then(user => {
+        if (!user) {
+          return res.status(401).json({
+            message: "Auth Failed"
+          });
+        }
+        fetchedUser = user;
+        return bcrypt.compare(req.body.password, user.password)
+      })
+      .then(result => {
+        if (!result) {
+          return res.status(401).json({
+            message: "Auth Failed"
+          });
+        }
+        const token = jwt.sign(
+          {email: fetchedUser.email, userId: fetchedUser._id},
+          'secret_this_should_be_longer',
+          { expiresIn: '1h' }
+        );
+        res.status(200).json({
+          token: token
+        });
+      })
+      .catch(err => {
+        res.status(401).json({
+          message: "Auth Failed"
+        });
+      });
   });
 
 module.exports = router;
